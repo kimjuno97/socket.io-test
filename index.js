cb3680dd17de6a0f3a4f86619612eef64fcd3366
kimjuno97 <jh241093@gmail.com>
2022-12-31 12:36:33 +0900

    socket.io 기본 기능 사용 후 저장

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', socket => {
+	console.log('연결 됨');
+	socket.on('chat message', msg => {
+		console.log('mesage: ', msg);
+	});
+
+	socket.on('disconnect', msg => {
+		console.log('연결 끊낌', msg);
+	});
+
+	socket.on('chat message', msg => {
+		io.emit('chat message', msg);
+	});
+});
+
+io.emit('some envent', {
+	someProperty: 'some value',
+	otherProperty: 'other value',
+});
+
+server.listen(3000, () => {
+	console.log('listening on *:3000');
+});
